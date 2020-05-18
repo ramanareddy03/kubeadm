d6fc51a46117a3d8b86a1b502237733407e2c53b
ramana <ramanareddi03@gmail.com>
2020-05-18 19:07:40 +0530

    kubeadm & docker installatio scripting

---
+++ docker_installation.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# installing docker CE on ubuntu 18.04
+### Install packages to allow apt to use a repository over HTTPS
+sudo apt-get update && apt-get install -y \
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
+
+# Add Docker’s official GPG key:
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+# Add the Docker apt repository:
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+# Install Docker CE
+sudo apt-get update && apt-get install -y \
+  containerd.io=1.2.13-1 \
+  docker-ce=5:19.03.8~3-0~ubuntu-$(lsb_release -cs) \
+  docker-ce-cli=5:19.03.8~3-0~ubuntu-$(lsb_release -cs)
+
+# Set up the Docker daemon
+sudo cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
